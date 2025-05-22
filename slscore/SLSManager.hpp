28cb3478de74dc25411f34c1b5da5ddeb4a9afbb
NerdySouth <46800978+NerdySouth@users.noreply.github.com>
2025-05-22 14:39:18 -0700

    Add CORS support

--- slscore/SLSManager.hpp
+++ slscore/SLSManager.hpp
@@ -47,6 +47,7 @@ int              worker_connections;
 char             stat_post_url[URL_MAX_LEN];
 int              stat_post_interval;
 char             record_hls_path_prefix[URL_MAX_LEN];
+char             cors_header[URL_MAX_LEN];
 SLS_CONF_DYNAMIC_DECLARE_END
 
 
@@ -61,6 +62,7 @@ SLS_SET_CONF(srt, int,    worker_connections,          "", 1, 1024),
 SLS_SET_CONF(srt, string, stat_post_url,               "statistic info post url", 1, URL_MAX_LEN-1),
 SLS_SET_CONF(srt, int,    stat_post_interval,          "interval of statistic info post.", 1, 60),
 SLS_SET_CONF(srt, string, record_hls_path_prefix,      "hls path prefix", 1, URL_MAX_LEN-1),
+SLS_SET_CONF(srt, string, cors_header,                 "CORS headers for HLS access", 1, URL_MAX_LEN-1),
 SLS_CONF_CMD_DYNAMIC_DECLARE_END
 
 
